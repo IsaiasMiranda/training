9812c085ac92ab9a0c42f320414230367c7dc6a6
IsaiasMiranda <isaias.analise@gmail.com>
2021-06-04 23:37:12 -0300

    Ajsute em classe employee

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -3,19 +3,25 @@ package entities;
 public class Employee {
 	private Integer id;
 	private String name;
+	private String email;
 	private Double salary;
 
 	public Employee() {
 	}
 
-	public Employee(Integer id, String name, Double salary) {
+	public Employee(Integer id, String name, String email, Double salary) {
 		this.id = id;
 		this.name = name;
+		this.email = email;
 		this.salary = salary;
 	}
 
 	public Integer getId() {
 		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -26,11 +32,19 @@ public class Employee {
 		this.name = name;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Double getSalary() {
 		return salary;
 	}
 
-	private void setSalary(Double salary) {
+	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
 
