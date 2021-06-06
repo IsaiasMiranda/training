ea4c4458c3ef8869b2b984caf6e04fc814971a94
isaiasmiranda <isaias.analise@gmail.com>
2021-06-05 22:57:18 -0300

    Ajustes em classe de BD

--- src/application/appDB.java
+++ src/application/appDB.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,13 +11,32 @@ public class appDB {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
-		if (conn != null) {
-			System.out.println("Conexão com o banco estabelecida!!!");
-		} else {
-			System.out.println("Erro ao conectar com o BD!!");
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+
+			String sql = "SELECT * FROM department";
+
+			rs = st.executeQuery(sql);
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " - " + rs.getString("name"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		DB.closeConnection();
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 
 	}
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -28,14 +30,14 @@ public class DB {
 
 	public static void closeConnection() {
 		try {
-			if(conn != null) {
+			if (conn != null) {
 				conn.close();
 			}
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
@@ -43,6 +45,26 @@ public class DB {
 			return props;
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
